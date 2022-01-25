00310ec16b63649590357ff336a1fbf5dee80ccc
Urara <zhufan131@163.com>
2022-01-25 22:19:15 +0800

    add simple signal handler

--- mws.h
+++ mws.h
@@ -1,13 +1,23 @@
+#ifndef MWS
+#define MWS 1
+
 #include <sys/types.h> /* basic system data types */
 #include <sys/socket.h> /* basic socket definitions */
+#include <sys/wait.h>
 #include <netinet/in.h> /* sockaddr_in{} and other Internet defns */
 #include <arpa/inet.h> /* inet(3) functions */
 #include <cerrno>
-#include <cstring>
+#include <cstring> //need bzero()
 #include <unistd.h>
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
+// #include <signal.h>
+#include <csignal> //C or C++ code style for UNIX lib func is confusing
+
+typedef void (*sighandler_t)(int);
+
 const int port = 6657; //不规范? //端口未来将被弃用
 const int maxline = 64;
 const int buffsize = 64;
+#endif
--- server.cpp
+++ server.cpp
@@ -1,9 +1,16 @@
 #include "./mws.h"
-
+void sighand_CHIL(int signo){
+    pid_t pid;
+    int stat;
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0)
+        std::cerr << pid << ':' << stat << std::endl;
+    return;
+}
 int main(int argc, char** argv){
     int listenfd, connfd;
     sockaddr_in servaddr;
     char buff[buffsize];
+
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -11,11 +18,22 @@ int main(int argc, char** argv){
     servaddr.sin_port = htons(port);
     bind(listenfd, (sockaddr*)&servaddr, sizeof(servaddr));
     listen(listenfd, 5);
+
+    sighandler_t sighand_CHIL_n = std::signal(SIGCHLD, sighand_CHIL);    //should update to sigaction() in future
+    if(sighand_CHIL_n == SIG_ERR){
+        // ?
+    }
+    
     for(;;){
         connfd = accept(listenfd, nullptr, nullptr);
         std::cout << "c" << std::endl;
         if(!fork()){
             close(listenfd);
+            bzero(buff, buffsize);
+            recv(connfd, buff, buffsize, 0);
+            std::cout << buff << std::endl;
+            send(connfd, buff, buffsize, 0);
+            
             bzero(buff, buffsize);
             recv(connfd, buff, buffsize, 0);
             std::cout << buff << std::endl;
